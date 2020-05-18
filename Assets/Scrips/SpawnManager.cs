16fcbf0ecd8809ff10755c5d2c8615337bffaf12
nicolalakpo <32912361+nicolalakpo@users.noreply.github.com>
2020-05-18 19:00:36 -0300

    failed
    hay que arreglar algo

--- Assets/Scrips/SpawnManager.cs
+++ Assets/Scrips/SpawnManager.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class SpawnManager : Photon.PunBehaviour
 {
     public List<GameObject> wayPoint = new List<GameObject>();
+    //public GameObject Ammunition;
+    float seconds = 0;
     void Start()
     {
+       
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        seconds += Time.deltaTime;
+        if (seconds > 2)
+        {
+            print("spanw");
+            jaja();
+            seconds = 0;
+        }
+
+    }
+    void jaja()
+    {
+        GameObject spawnAmmunition = PhotonNetwork.Instantiate("muni", wayPoint[Random.Range(0, wayPoint.Count)].transform.position, Quaternion.identity, 1);
+
     }
 }
