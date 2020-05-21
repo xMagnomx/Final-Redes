767c211b6b1c2a1d84ea51aee3caee73f2900aad
nicolalakpo <32912361+nicolalakpo@users.noreply.github.com>
2020-05-21 15:58:06 -0300

    ammuniton
     detruir minicion

---
+++ Assets/Scrips/Ammunition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammunition : Photon.PunBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            //tiene que da municion
+            Destroy(gameObject);
+    }
+}
